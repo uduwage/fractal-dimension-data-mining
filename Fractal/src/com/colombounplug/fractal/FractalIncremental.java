42c06a1ef538715ffa7b42cf0c7979647ff5ec6f
auduwage <auduwage@ed94dde4-af99-11de-b3ee-c79121bffc40>
2009-12-18 16:10:58 +0000

    Holding place for deleted test methods

--- Fractal/src/com/colombounplug/fractal/FractalIncremental.java
+++ Fractal/src/com/colombounplug/fractal/FractalIncremental.java
@@ -3,10 +3,46 @@
  */
 package com.colombounplug.fractal;
 
+import java.util.ArrayList;
+
 /**
  * @author anuradha.uduwage
  *
  */
 public class FractalIncremental {
 
+	/*
+	public void dfsNearestNeighbor(double point, ArrayList<Double> list, boolean[] pointIsVisited ) {
+		if (point != 0.0) {
+			list.add(point);
+			cluster.add(point);
+			pointIsVisited[(int) point] = true;
+			double newNeighbor =  nearestNeighbor(point);
+			if (newNeighbor !=  0.0) {
+				cluster.add(newNeighbor);
+				for (int i = 0; i < tempList.size(); i++) {
+					if (!pointIsVisited[i]) {
+						dfsNearestNeighbor(newNeighbor, list, pointIsVisited);
+					}
+				}
+			}
+		}
+	}
+	
+	
+	//Distance grid.
+	public double[][] distanceGrid() {
+		double[] gridSize = combineArrays(generateClusters(1, 3), generateClusters(12, 15));
+		double [][] pointsDistanceGrid = new double[gridSize.length][gridSize.length];
+		for (int i = 0; i < pointsDistanceGrid.length; i++) {
+			for (int j = 0; j < pointsDistanceGrid[i].length; j++) {
+				pointsDistanceGrid[i][j] = Math.abs(gridSize[i] - gridSize[j] );
+				System.out.print(" " + pointsDistanceGrid[i][j]);
+			}
+			System.out.println("");
+		}
+
+		return pointsDistanceGrid;
+	}	
+	*/	
 }
